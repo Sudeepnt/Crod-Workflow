9f129d0339afda56d52c07428f971c609bbbc92a
Sudeep N T <sudeepnt@Macmini.lan>
2026-07-22 20:00:40 +0530

    Disconnect old Supabase project

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.WORKFLOW_CONFIG = {
-  supabaseUrl: "https://zymwuvjsisuoqmqnjrhc.supabase.co",
-  supabasePublishableKey: "sb_publishable_lXYAfjA7C-Z_745HazgaXg_4EAPZmvF",
+  supabaseUrl: "",
+  supabasePublishableKey: "",
 };
