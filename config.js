be93fe2c2d04915fab4a18b3e752a33e857841a8
Sudeep N T <sudeepnt@Macmini.lan>
2026-07-22 20:08:37 +0530

    Connect app to new Supabase project

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.WORKFLOW_CONFIG = {
-  supabaseUrl: "",
-  supabasePublishableKey: "",
+  supabaseUrl: "https://ejuynlfumyvriufxnzam.supabase.co",
+  supabasePublishableKey: "sb_publishable_gYutqzoCmYWw8YLQ6bEYLw_K0P_xH0G",
 };
